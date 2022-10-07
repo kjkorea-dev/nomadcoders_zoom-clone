eae0fed79488a4b4b47d789710cb07c87a77d96a
kj-dev <agassi77.dev@gmail.com>
2022-10-07 13:56:00 +0900

    SocketIO rooms

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,15 +1,24 @@
 const socket = io();
 
 const welcome = document.getElementById("welcome");
+const room = document.getElementById("room");
 const form = welcome.querySelector("form");
 
-function backendDone(msg) {
-  console.log(`The backend says: ${msg}`);
+room.hidden = true;
+
+let roomName;
+
+function showRoom() {
+  room.hidden = false;
+  welcome.hidden = true;
+  const h3 = document.querySelector("h3");
+  h3.innerText = `Room: ${roomName}`;
 }
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   const input = form.querySelector("input[type=text]");
-  socket.emit("enter_room", input.value, backendDone);
+  roomName = input.value;
+  socket.emit("enter_room", input.value, showRoom);
   input.value = "";
 });
